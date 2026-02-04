53ead8b80a053f0ff85ae76dab68e76c9d8e7f8f
GustavoMelo1 <gustavomelo.oliveira@hotmail.com>
2026-02-04 20:06:16 -0300

    Explicando sobre Variaveis e Exemplos

---
+++ studies-python/variablespy/variables.py
@@ -0,0 +1,22 @@
+"INICIO DO ROAD MAP DE TODAS AS COISAS DE PYTHON"
+
+"VARIAVEL BASICAMENTE GUARDA OS VALORES"
+
+"EXEMPLOS"
+
+
+#TEXTO (String). Ex: Nome
+#Inteiro: Ex: Idade
+#Float. Ex: Altura
+#Logica: Ex: Falso(Transação fraudulenta)
+
+
+dinheiro = 1250
+porcentagem_taxa = 0.1
+total_taxa_mes = dinheiro * porcentagem_taxa
+imposto = total_taxa_mes
+
+print (imposto)
+
+
+
