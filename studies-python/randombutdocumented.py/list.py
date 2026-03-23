e864ab344e2749642c16b3a6ef04123a237f28c7
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-03-23 20:50:24 -0300

    feat : adding a file "list" and "string" to the stuties path

---
+++ studies-python/randombutdocumented.py/list.py
@@ -0,0 +1,8 @@
+#LISTS
+
+my_list = ['a', 'b', 'c', 'd', 'e']
+
+my_list.append('f') # adding a 'f' to the list
+print(my_list) # ['a', 'b', 'c', 'd', 'e', 'f']
+
+print(my_list[0:3]) # ['a', 'b', 'c']
---
+++ studies-python/randombutdocumented.py/strings.py
@@ -0,0 +1,9 @@
+#STRINGS
+
+s = 'abcdefg'
+
+s[0:] # 'abcdefg'
+s[0:4] # 'abcd'
+s[0:7:2] # 'aceg'
+
+
