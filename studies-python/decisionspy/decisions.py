a75cae37711d64e83a830f17b80b501e092c6a65
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-03-23 19:48:16 -0300

    feat: implements exercises and examples of decision structures

---
+++ studies-python/decisionspy/decisions.py
@@ -0,0 +1,30 @@
+#ESTRUTURAS DE DECISÕES 
+
+# if se ...
+# else caso contrario
+# elif meio termo de ambos
+
+nota = 4
+if nota >= 7 :
+    print("Aprovado")
+
+else:
+    print ("Reprovado")
+
+
+if nota <= 4 :
+    print("Reprovado")
+elif nota > 4 and nota <=6 :
+    print("Exame")
+else:
+    print("Aprovado")
+
+
+gustavo = 7
+
+if gustavo == 4:
+    print("Vai ficar em casa")
+elif 4 < gustavo <= 6:
+    print("Pensar")
+else:
+    print("Vai pro carnaval")
